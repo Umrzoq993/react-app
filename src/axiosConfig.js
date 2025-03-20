48191648278728188f72d02e7b818d43131392dc
root <root@vmi2515644.contaboserver.net>
2025-03-20 23:54:41 +0500

    Update package.json and axiosConfig for server config

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { navigate } from "./NavigationService"; // adjust the path as needed
 
 const axiosInstance = axios.create({
-  baseURL: "https://173.249.1.220:8443/api",
+  baseURL: "http://173.249.1.220/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
